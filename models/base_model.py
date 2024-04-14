a576594e83826f1ab4404f4a231ca1af2ec93bd5
Dexterski007 <mbouj007@gmail.com>
2024-04-14 14:48:27 -0700

    fixing_stuff

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,18 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if k == "updated_at" or k == "created_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.today()
+            if "created_at" not in kwargs:
+                self.created_at = datetime.today()
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import shlex
 
 
 class FileStorage:
@@ -8,9 +16,20 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dic = {}
+        if not cls:
+            return self.__objects
+        else:
+            newdict = self.__objects
+            for k in newdict:
+                param = k.replace('.', ' ')
+                param = shlex.split(param)
+                if (param[0] == cls.__name__):
+                    dic[k] = self.__objects[k]
+            return dic
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,13 +46,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
