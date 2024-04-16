25b6d7d3c96bad993d12cf9ad6718a3eb1b7a11d
Dexterski007 <mbouj007@gmail.com>
2024-04-16 14:57:09 -0700

    hey

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete-orphan',
+                          backref="user")
