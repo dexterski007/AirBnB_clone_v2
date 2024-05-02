4922f07c546524457ccac86db23fe8a6ce6bd472
Dexterski007 <mbouj007@gmail.com>
2024-05-02 14:26:15 -0700

    hey

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #script to prepare for web static
 
-sudo apt -y update
+sudo apt update
 sudo apt -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/releases/test/
@@ -9,8 +9,8 @@ sudo mkdir -p /data/web_static/shared/
 sudo sh -c 'echo "Hello World!" > /data/web_static/releases/test/index.html'
 sudo ln -fs /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-if ! grep -q "location hbnb_static" "/etc/nginx/sites-available/default"; then
-	sudo sed -i '/root \/var\/www\/html/{;a location hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}
+if ! grep -q "location /hbnb_static" "/etc/nginx/sites-available/default"; then
+	sudo sed -i '/root \/var\/www\/html/{;a location /hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}
 }' /etc/nginx/sites-available/default
 fi
 sudo systemctl restart nginx
