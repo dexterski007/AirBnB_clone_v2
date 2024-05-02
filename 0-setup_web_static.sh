8cc349b39747d4ee200785eed63d8823b6e32581
Dexterski007 <mbouj007@gmail.com>
2024-05-02 15:37:37 -0700

    nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,20 +3,17 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo sh -c 'echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+</html>" > /data/web_static/releases/test/index.html'
+rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
