4d0f2e5d46ae1f656682a68eda0cb4f36276f929
Dexterski007 <mbouj007@gmail.com>
2024-05-02 14:40:19 -0700

    freemix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,15 @@ sudo apt -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo sh -c 'echo "Hello World!" > /data/web_static/releases/test/index.html'
-sudo ln -fs /data/web_static/releases/test /data/web_static/current
+sudo sh -c 'echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html'
+rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 if ! grep -q "location /hbnb_static" "/etc/nginx/sites-available/default"; then
 	sudo sed -i '/root \/var\/www\/html/{;a location /hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}
