26603295dc916b8e1644f74e03cc847c7829fdad
Dexterski007 <mbouj007@gmail.com>
2024-05-02 15:35:14 -0700

    hey

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,8 +16,7 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-sudo systemctl restart nginx
+sudo service nginx restart
