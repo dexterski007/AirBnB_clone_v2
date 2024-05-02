f46dfa8bfeae463edc6cdc2f52d164a3aafe029f
Dexterski007 <mbouj007@gmail.com>
2024-05-02 13:42:10 -0700

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,8 +10,7 @@ sudo sh -c 'echo "Hello World!" > /data/web_static/releases/test/index.html'
 sudo ln -fs /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 if ! grep -q "location hbnb_static" "/etc/nginx/sites-available/default"; then
-	sudo sed -i '/root \/var\/www\/html/{;a \
-    location hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}
+	sudo sed -i '/root \/var\/www\/html/{;a location hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}
 }' /etc/nginx/sites-available/default
 fi
 sudo systemctl restart nginx
