d695d1850562958e6568592e9f7e33f6522fee51
Dexterski007 <mbouj007@gmail.com>
2024-05-03 14:06:17 -0700

    ubuntu

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -18,11 +18,16 @@ file { [
   '/data/web_static/releases/test/',
 ]:
   ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
 file { '/data/web_static/current':
   ensure  => link,
   target  => '/data/web_static/releases/test/',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static/releases/test/'],
 }
 
 file { '/data/web_static/releases/test/index.html':
