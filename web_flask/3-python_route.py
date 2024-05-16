1bf89dc2f8dcc9175bc00fef5ae7600c0114ccd2
dexterski007 <mbouj007@gmail.com>
2024-05-16 14:38:14 -0700

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,12 @@ def cisfun(text):
     """ c + fun"""
     return f'C {text.replace("_"," ")}'
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text="is cool"):
+    """ python is cool"""
+    return f'Python {text.replace("_"," ")}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
