648188cdf50cbda813d64aea5f466e97f9f665f5
dexterski007 <mbouj007@gmail.com>
2024-05-16 14:20:53 -0700

    2nd

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,10 @@ def hello_world():
     """ hello world"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    """ hello world"""
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
