8c4f7068c97890e9b85a2d16eb44bdd582571cd8
dexterski007 <mbouj007@gmail.com>
2024-05-17 10:43:57 -0700

    test

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -11,5 +12,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all,\
-                          delete-orphan', backref="cities")
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
